2d7412eaf195284029f59ca140268eb11fb37b58
18008668090 <31022420+18008668090@users.noreply.github.com>
2019-07-04 16:47:10 +0800

    Update travel.js

--- resources/static/js/travel.js
+++ resources/static/js/travel.js
@@ -40,6 +40,32 @@ $(document).ready(function () {
     //比例尺
     L.control.scale({ "imperial": false }).addTo(_mymap);
 
+    var lines=[];
+    lines.push("30.447951 114.360457");
+    lines.push("39.849574 116.407665");
+    lines.push("19.600291 110.228352");
+    lines.push("30.561921 111.275854");
+    lines.push("38.924682 117.130589");
+    lines.push("28.998176 110.309634");
+    lines.push("28.929823 111.660139");
+    var tips = "这我去过";
+    for (var i = 0; i < lines.length; i++) {
+        var latlng0 = lines[i].split(" ");
+        if (latlng0.length > 1) {//一行两个数字
+            var marker = new L.marker([latlng0[0], latlng0[1]], {  })
+                .addTo(_mymap)
+                .bindPopup(tips);
+        }
+    }
+//点击位置
+//     var popup = L.popup();
+//     function onMapClick(e) {
+//        popup
+//            .setLatLng(e.latlng)
+//            .setContent("You clicked the map at " + e.latlng.toString())
+//            .openOn(_mymap);
+//     }
+//     _mymap.on('click', onMapClick);
 });
 //控制容器大小
 function PanelResize() {
